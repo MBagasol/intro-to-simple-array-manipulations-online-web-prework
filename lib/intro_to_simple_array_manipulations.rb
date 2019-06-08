4ee8c6bebdf8954ac9929c848a22b273f8e5ee53
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-08 03:43:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,25 @@
-def using_array(array, string) 
-  
+def using_push(array, string) 
+  array.push(string)
 end
 
 def using_unshift(array)
-  
+  array.unshift
 end
 
 def using_pop(array)
-  
+  array.pop
 end
 
 def pop_with_args(array)
-  
+  array.pop(1)
 end
 
 def using_shift(array) 
-  
+  array.shift
 end
 
 def shift_with_args(array)
-  
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
@@ -31,11 +31,11 @@ def using_insert(array, item)
 end
 
 def using_uniq(array)
-  
+  array.uniq
 end
 
 def using_flatten(array)
-  
+  arra.flatten
 end
 
 def using_delete(array, string)
